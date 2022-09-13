4c38a897c28d53ce417fe2b7fa55ca33e7767bde
ChauKimSon <113006711+ChauKimSon@users.noreply.github.com>
2022-09-13 19:39:21 +0700

    first commit

---
+++ bin/dart_text.dart
@@ -0,0 +1,6 @@
+void main(List<String> arguments) {
+  int a = 0;
+  if (a == 0) {
+    a = b + c;
+  }
+}
---
+++ test/dart_text_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_text/dart_text.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
